43984255e3de1d6f1c8c41988d3e1ed3caacf999
Ankit Kumar <ankitsingh17082003@gmail.com>
2025-11-09 11:18:53 +0530

    Add src/components/layout/Footer.jsx

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-white border-t py-4 text-center">
+      <div className="container mx-auto">
+        <p className="text-sm text-gray-500">&copy; 2023 Todo App. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
