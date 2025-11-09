b78a20b19608d1959a54d4c2a127c80d547902cd
Ankit Kumar <ankitsingh17082003@gmail.com>
2025-11-09 11:18:49 +0530

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#3b82f6',
+      },
+    },
+  },
+  plugins: [],
+};
